dc9482cf1fb87244e78929cd4d0bc57ac123da67
v-rane28 <vaibhavrane2883@gmail.com>
2026-09-02 15:11:42 +0530

    Exceptiom

--- C#Practical/Array_Excp.cs
+++ C#Practical/Array_Excp.cs
@@ -16,14 +16,14 @@ class Array_Excp
             nums[1] = 33.7;
             nums[2] = 48.9;
         }
-        //catch
-        //{
-        //    Console.WriteLine("Element Out OF the Box Exeception");
-        //    for (int i = 0; i < nums.Length; i++)
-        //    {
-        //        Console.WriteLine(nums[i]);
-        //    }
-        //}
+        catch
+        {
+            Console.WriteLine("Element Out OF the Box Exeception");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+        }
         finally
         {
             for (int i = 0; i < nums.Length; i++)
